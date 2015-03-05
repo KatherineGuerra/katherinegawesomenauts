e8ff6e4f15c104c8ad3db0f2866a7a34d87086ff
katherine guerra <guerrakn@.gmail>
2015-03-05 11:29:28 -0800

    commit

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -1,1 +1,21 @@
-// TODO
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, 'init', [x, y, {
+                image: "player",
+                /*the width and height are telling the screen the amount of space to preserve*/
+                width: 64,
+                height: 64,
+                /*spritewidth and spriteheight are passing information*/
+                spritewidth: "64",
+                spriteheight: "64",
+                getShape: function(){
+                    return(new me.Rect(0,0, 64, 64)).toPolygon();
+                }
+        }])
+    },
+    
+    update: function(){
+        
+        
+    }
+});
