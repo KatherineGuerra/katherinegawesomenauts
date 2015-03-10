4b2efb7d237124759e5290e765e595273ef7fad2
katherine guerra <guerrakn@.gmail>
2015-03-10 11:28:37 -0700

    3-10--15

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -14,6 +14,8 @@ game.PlayerEntity = me.Entity.extend({
         }]);
     
            this.body.setVelocity(5, 20);
+           //this means that the screen will follow where ever the player goes
+           me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
            
            this.renderable.addAnimation("idle", [78]);
            this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 122, 123, 124, 125], 80);
@@ -69,10 +71,15 @@ game.PlayerBaseEntity = me.Entity.extend({
         this.body.onCollision = this.onCollision.bind(this);
         
         this.type = "PlayerBaseEntity";
+        
+        this.renderable.addAnimation("idle", [0]);
+        this.renderable.addAnimation("broken", [1]);
+        this.renderable.setCurrentAnimation("idle");
     },
     update: function(delta){
         if(this.health<=0){
             this.broken = true;
+            this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
         
@@ -103,10 +110,15 @@ game.EnemyBaseEntity = me.Entity.extend({
         this.body.onCollision = this.onCollision.bind(this);
         
         this.type = "EnemyBaseEntity";
+        
+        this.renderable.addAnimation("idle", [0]);
+        this.renderable.addAnimation("broken", [1]);
+        this.renderable.setCurrentAnimation("idle");
     },
     update: function(delta){
         if(this.health<=0){
             this.broken = true;
+            this.renderable.setCurrentAnimation("broken");
         }
         this.body.update(delta);
         
