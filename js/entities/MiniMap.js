a27557ac6383f72b0d90b021c802defd47b9dc62
katherine guerra <guerrakn@.gmail>
2015-04-27 12:14:25 -0700

    4-27-15

--- js/entities/MiniMap.js
+++ js/entities/MiniMap.js
@@ -1,7 +1,15 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
+game.MiniMap = me.Entity.extend({
+    init: function(x, y, settings){
+        this._super(me.Entity, "init", [x, y, {
+                image: "minimap",
+                width: 282,
+                height: 157,
+                spritewidth: "282",
+                spriteheight: "157",
+                getShape: function(){
+                    return (new me.Rect(0, 0, 282, 157)).toPolygon();
+                }
+        }]);
+        this.floating = true;
+    }
+});
---
+++ js/entities/MiniPlayerLocation.js
@@ -0,0 +1,12 @@
+game.MiniPlayerLocation = me.Entity.extend({
+    init: function(x, y, settings){
+        
+    },
+    
+    draw: function(){
+        
+    },
+    update: function(){
+        
+    }
+});
